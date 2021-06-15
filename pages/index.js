8b11ab5d7d45c0de0f2036eb0b679c81b4d951c6
fiiv <git@fiiv.io>
2021-06-15 15:13:14 +0700

    working bingo game

--- pages/index.js
+++ pages/index.js
@@ -4,8 +4,17 @@ import { useRouter } from 'next/router'
 import { randomNumber, slugify, deslugify } from '../lib/helpers'
 import squareSource from '../_data/squares'
 
+const BingoSquare = ({ id, label, checked, onClick, isClickable = true } = {}) => (
+  <button
+    className={`h-24 flex items-center justify-center p-2 ${isClickable ? 'cursor-pointer' : 'cursor-default'} ${checked ? 'bg-red-700' : 'bg-white'}`}
+    onClick={() => isClickable && onClick(id)}>
+    <p className={`text-center text-lg ${checked ? 'text-white' : 'text-red-700'}`}>{label}</p>
+  </button>
+)
+
 export default function Home(props) {
   const [ squares, setSquares ] = useState([])
+  const [ checked, setChecked ] = useState([])
   const [ slug, setSlug ] = useState()
 
   useEffect(() => {
@@ -14,13 +23,13 @@ export default function Home(props) {
     let slugString = ''
 
     while (shuffled.length <= 24) {
-      if (shuffled.length === 12) shuffled.push({ label: 'FREE', id: 'FREE' })
+      if (shuffled.length === 12) shuffled.push({ label: 'FREE', id: 'FREE', checked: true, isClickable: false })
       else {
         const index = randomNumber(0, squareSource.length - 1)
         const square = squareSource[index]
 
         if (!used.includes(square.id)) {
-          shuffled.push({ ...square })
+          shuffled.push({ ...square, isClickable: true })
           slugString += slugify(index)
           used.push(square.id)
         }
@@ -31,22 +40,30 @@ export default function Home(props) {
     setSlug(slugString)
   }, [])
 
+  const onClickSquare = clickedId => {
+    const isChecked = checked.includes(clickedId)
+
+    if (isChecked) setChecked(checked.filter((id) => id !== clickedId))
+    else setChecked([ ...checked, clickedId ])
+  }
+
   return (
     <main className="bg-gradient-to-t from-yellow-400 via-red-500 to-pink-500 w-screen h-screen">
       <div className="w-full h-full flex items-center justify-center">
         <div className="bg-white w-1/2 shadow-2xl rounded-lg">
-          <h1 className="text-6xl uppercase text-center text-white py-4 bg-black rounded-t-lg">Meeting Hell Bingo</h1>
+          <h1 className="text-6xl uppercase text-center text-white py-4 bg-red-700 rounded-t-lg">Meeting Hell Bingo</h1>
 
-          <div className="grid grid-cols-5 grid-flow-row gap-2 bg-black px-2">
-            {squares.map(({ id, label }, i) => (
-              <div className="h-24 flex items-center justify-center bg-white p-2" key={id}>
-                <p className="text-center text-lg">{label}</p>
-              </div>
+          <div className="grid grid-cols-5 grid-flow-row gap-2 px-2">
+            {squares.map((props) => (
+              <BingoSquare
+                {...props}
+                key={props.id}
+                checked={props.id === 'FREE' || checked.includes(props.id)}
+                onClick={onClickSquare} />
             ))}
           </div>
 
-          <div className="rounded-b-lg py-4 bg-black">
-            <p className="text-white">{slug}</p>
+          <div className="rounded-b-lg py-4">
           </div>
         </div>
       </div>
